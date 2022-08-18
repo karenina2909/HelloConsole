f3404c62bbc0cf8734c52e2e068345737c7034d2
Светлана М <karenina2909@mail.ru>
2022-08-19 00:11:38 +0300

    гири

--- Задача с гирями/Program.cs
+++ Задача с гирями/Program.cs
@@ -9,7 +9,9 @@ int max = a;
 if (a > max ) max = a;
 if (b > max ) max = b;
 if (c > max ) max = c;
+if (d > max ) max = d;
 if (e > max ) max = e;
+
 
 Console.Write("max = ");
 Console.WriteLine(max); 
