0ab3ca91ffea1038bf5a00ae3ab84527b1a8ac82
Светлана М <karenina2909@mail.ru>
2022-08-21 14:17:57 +0300

    задача с точками

---
+++ Задача с точками/Program.cs
@@ -0,0 +1,29 @@
+﻿ //Console.Clear();
+ //Console.SetCursorPosition(1,4);
+ //Console.WriteLine("+");
+  
+int xa = 40, ya = 1,
+    xb = 1, yb = 30,
+    xc = 60, yc =30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int count =0;
+while(count < 10)
+{
+    int what = new Random().Next(0,3);//012
+    if (what ==0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+
+    
+}
